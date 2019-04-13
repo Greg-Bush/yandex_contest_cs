2b1acbeb1b0e045384f042d02712bf71c3725a92
Greg-Bush <greg.bush.main@icloud.com>
2019-04-14 00:27:46 +0300

    the task about dublicates

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace yandex_contest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = readNextNumber();
+            if (n < 1)
+            {
+                return;
+            }
+            int previousNumber = readNextNumber();
+            output(previousNumber);
+            int i = 1;
+            while (i < n)
+            {
+                int number = readNextNumber();
+                if (number != previousNumber)
+                {
+                    output(number);
+                    previousNumber = number;
+                }
+                i++;
+            }
+        }
+
+        private static int readNextNumber()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+        private static void output(object value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
